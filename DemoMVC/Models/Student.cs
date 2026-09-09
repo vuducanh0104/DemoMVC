02d028e2713a464ef6fda457ffaf1339866ad3bf
VUDUCANH <anhmin1012@gmail.com>
2026-09-09 23:11:46 +0700

    update

--- DemoMVC/Models/Student.cs
+++
@@ -1,8 +0,0 @@
-namespace DemoMVC.Models;
-
-public class Student
-{
-    public string FullName { get; set; } = string.Empty;
-    public string Addres { get; set; } = string.Empty;
-    public string University { get; set; } = string.Empty;
-}
